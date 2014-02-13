9a69cb9b79906fd7f256baf1918071c379a37674
Erik <erik@letterror.com>
2014-02-13 16:57:24 +0100

    Overprinter
    This will grow to be a script that shows overprinting samples.

--- code/overprinter.py
+++ code/overprinter.py
@@ -11,20 +11,20 @@ from robofab.world import OpenFont
 import os
 
 
-def drawMultiply():
-    image = NSImage.alloc().initWithSize_(size)
-    image.lockFocus()
-    NSColor.whiteColor().set()
-    NSRectFill(NSRect((0,0), size))
-    gc = NSGraphicsContext.currentContext()
-    gp = gc.graphicsPort()
-    CGContextSetBlendMode(gp, kCGBlendModeMultiply)
-    #gc.setCompositingOperation_(NSCompositePlusDarker)
-    gradient = NSGradient.alloc().initWithColors_([verticalColor, NSColor.clearColor()])
-    gradient.drawFromPoint_toPoint_options_((0,0), (0,size[1]), 0)
-    gradient = NSGradient.alloc().initWithColors_([horizontalColor, NSColor.clearColor()])
-    gradient.drawFromPoint_toPoint_options_((0,0), (size[0],0), 0)
-    image.unlockFocus()
+#def drawMultiply():
+#    image = NSImage.alloc().initWithSize_(size)
+#    image.lockFocus()
+#    NSColor.whiteColor().set()
+#    NSRectFill(NSRect((0,0), size))
+#    gc = NSGraphicsContext.currentContext()
+#    gp = gc.graphicsPort()
+#    CGContextSetBlendMode(gp, kCGBlendModeMultiply)
+#    #gc.setCompositingOperation_(NSCompositePlusDarker)
+#    gradient = NSGradient.alloc().initWithColors_([verticalColor, NSColor.clearColor()])
+#    gradient.drawFromPoint_toPoint_options_((0,0), (0,size[1]), 0)
+#    gradient = NSGradient.alloc().initWithColors_([horizontalColor, NSColor.clearColor()])
+#    gradient.drawFromPoint_toPoint_options_((0,0), (size[0],0), 0)
+#    image.unlockFocus()
 
 
 ufoPath = u"wood/TypeMedia1314-WoodType.ufo"
@@ -33,3 +33,17 @@ print os.path.exists(ufoPath)
 
 f = OpenFont(ufoPath)
 print f
+
+cm = 28.3464567    # points in 1 centimeter
+
+#a6width = 14.8*cm
+#a6height = 10.5*cm
+#size(a6width, a6height)
+size(1000,1000)
+#translate(100,100)
+rect(10, 10, 100, 100)
+
+c = CocoaPen(f)
+f["Ocounter"].draw(c)
+c.path.fill()
+
