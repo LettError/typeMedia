a7412aa3c01cd245499d1b1b1dfa0f7a062cfc10
Erik <erik@letterror.com>
2014-02-20 09:33:02 +0100

    Overprinter
    This now shows a grid of all glyphs stacked, with overprinting effects.
    Woot.

--- code/overprinter.py
+++ code/overprinter.py
@@ -35,6 +35,36 @@ def drawLetter(f, glyphName, fontSize):
     drawPath(c.path)
     restore()
 
+def drawOverprintLetter(items, fontSize):
+    """ Let's put all the drawable items in a dict so that we can stack more than 2 if we want to. 
+    
+    This needs the new Drawbot to work. :)
+    """
+    maxGlyphWidth = None
+    glyphHeight = fontSize
+    for item in items:
+        w = item['font'][item['glyphName']].width
+        if maxGlyphWidth is None:
+            maxGlyphWidth = w
+        maxGlyphWidth = max(maxGlyphWidth, w)
+    imageSize = (maxGlyphWidth, glyphHeight)
+    overprintImage = NSImage.alloc().initWithSize_(imageSize)
+    overprintImage.lockFocus()
+    for item in items:
+        save()
+        f = item['font']
+        color = item['color']
+        fill(color[0], color[1], color[2], color[3])
+        fontScale = float(fontSize) / f.info.unitsPerEm
+        scale(fontScale)
+        c = CocoaPen(f)
+        f[item["glyphName"]].draw(c)
+        drawPath(c.path)
+        restore()
+    overprintImage.unlockFocus()
+    image(overprintImage, (0,0))
+    
+
 
 
 ufoPath = u"wood/TypeMedia1314-WoodType.ufo"
@@ -42,6 +72,8 @@ ufoPath = os.path.join(os.path.dirname(os.getcwd()), ufoPath)
 print os.path.exists(ufoPath)
 
 f = OpenFont(ufoPath)
+names = f.keys()
+print names
 print f
 
 cm = 28.3464567    # points in 1 centimeter
@@ -49,12 +81,22 @@ cm = 28.3464567    # points in 1 centimeter
 #a6width = 14.8*cm
 #a6height = 10.5*cm
 #size(a6width, a6height)
-size(1000,1000)
-translate(50,400)
+size(3000,1000)
+fontSize = 15
 
-fontSize = 200
-fill(1,0,0.0,.6)
-drawLetter(f, "one_unit", fontSize)
-fill(1,1,0.0,.6)
+translate(50,1000-2*fontSize)
 
-drawLetter(f, "Acounter", fontSize)
+gridWidth = 1414 * fontSize / f.info.unitsPerEm
+for firstName in names:
+    save()
+    for secondName in names:
+        items = [
+            dict(font=f, color=(0,0,1.0,.6), glyphName=firstName),
+            dict(font=f, color=(0.5,0,0.0,.6), glyphName=secondName),
+        
+        ]
+        drawOverprintLetter(items, fontSize)
+        translate(gridWidth+10,0)
+    restore()
+    translate(0,-fontSize*1.4)
+    
