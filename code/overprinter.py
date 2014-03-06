e28b0ed1ed79b20ef5823a624e3952a84dbedc50
Erik <erik@letterror.com>
2014-03-06 09:26:26 +0100

    pdfs

--- code/overprinter.py
+++ code/overprinter.py
@@ -5,6 +5,8 @@
 
 """
 from AppKit import NSImage, NSGraphicsContext, NSRectFill, NSColor, NSGradient
+from Quartz import *
+
 from fontTools.pens.cocoaPen import CocoaPen
 from robofab.world import OpenFont
 
@@ -35,7 +37,7 @@ def drawLetter(f, glyphName, fontSize):
     drawPath(c.path)
     restore()
 
-def drawOverprintLetter(items, fontSize):
+def drawOverprintLetter(items, fontSize, unitsPerEm):
     """ Let's put all the drawable items in a dict so that we can stack more than 2 if we want to. 
     
     This needs the new Drawbot to work. :)
@@ -47,21 +49,31 @@ def drawOverprintLetter(items, fontSize):
         if maxGlyphWidth is None:
             maxGlyphWidth = w
         maxGlyphWidth = max(maxGlyphWidth, w)
-    imageSize = (maxGlyphWidth, glyphHeight)
+    imageSize = (maxGlyphWidth*fontSize, unitsPerEm)
     overprintImage = NSImage.alloc().initWithSize_(imageSize)
     overprintImage.lockFocus()
+    fontScale = float(fontSize) / unitsPerEm
+    aT = NSAffineTransform.transform()
+    aT.scaleBy_(fontScale)
+    aT.concat()
+
+    #gc = NSGraphicsContext.currentContext()
+    #gp = gc.graphicsPort()
+    #CGContextSetBlendMode(gp, kCGBlendModeMultiply)
     for item in items:
         save()
         f = item['font']
         color = item['color']
-        fill(color[0], color[1], color[2], color[3])
-        fontScale = float(fontSize) / f.info.unitsPerEm
-        scale(fontScale)
+        #fill(color[0], color[1], color[2], color[3])
+        nsc = NSColor.colorWithDeviceRed_green_blue_alpha_(color[0],color[1],color[2],color[3])
+        nsc.set()
         c = CocoaPen(f)
         f[item["glyphName"]].draw(c)
-        drawPath(c.path)
+        c.path.fill()
+        #drawPath(c.path)
         restore()
     overprintImage.unlockFocus()
+    # get drawbot to draw the image.
     image(overprintImage, (0,0))
     
 
@@ -72,7 +84,10 @@ ufoPath = os.path.join(os.path.dirname(os.getcwd()), ufoPath)
 print os.path.exists(ufoPath)
 
 f = OpenFont(ufoPath)
-names = f.keys()
+#names = f.keys()
+names = [ "D", "F", "H", "N", "S", "Z", "five", "eight", "Gcounter", "one_unit", "Acounter", "Ocounter", "ecounter", "half_unit", "Xcounter", "Ccounter"]
+
+names = [ "D", "F"]
 print names
 print f
 
@@ -82,7 +97,7 @@ cm = 28.3464567    # points in 1 centimeter
 #a6height = 10.5*cm
 #size(a6width, a6height)
 size(3000,1000)
-fontSize = 15
+fontSize = 100
 
 translate(50,1000-2*fontSize)
 
@@ -91,11 +106,9 @@ for firstName in names:
     save()
     for secondName in names:
         items = [
-            dict(font=f, color=(0,0,1.0,.6), glyphName=firstName),
-            dict(font=f, color=(0.5,0,0.0,.6), glyphName=secondName),
-        
+            dict(font=f, color=(0,0,random(),0.5), glyphName=choice(names)),
         ]
-        drawOverprintLetter(items, fontSize)
+        drawOverprintLetter(items, fontSize, f.info.unitsPerEm)
         translate(gridWidth+10,0)
     restore()
     translate(0,-fontSize*1.4)
